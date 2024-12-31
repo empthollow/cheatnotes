43393ddd2075b04ace0203146e9eb9f3399b60f7
empthollow <empthollow@gmail.com>
2024-12-31 17:30:16 -0600

    99 theme updates

--- prompt.sh
+++ prompt.sh
@@ -1,10 +1,22 @@
 #/bin/bash
 # place in /etc/profile.d/
+# delete irrelevant themes 
 
-# server machine user colors
-# PS1="\[\e[1;33m\]\u\[\e[1;32m\]@\h \[\e[1;32m\]\W \$\[\e[m\] "
+# physical server theme
+if [ "$(id -u)" == "0" ] ; then
+	PS1='\[\e[1;31m\]\u\[\e[1;32m\]@\h\[\e[1;31m\] \W \$\[\e[m\] '
+else
+	PS1="\[\e[1;33m\]\u\[\e[1;32m\]@\h \[\e[1;32m\]\W \$\[\e[m\] "
+fi
 
-# client machine colors
+# VM theme 
+if [ "$(id -u)" == "0" ] ; then
+        PS1='\[\e[1;31m\]\u\[\e[1;33m\]@\h\[\e[1;33m\] \W \$\[\e[1;33m\] '
+else
+        PS1="\[\e[1;35m\]\u\[\e[1;33m\]@\h \[\e[1;33m\]\W \$\[\e[1;33m\] "
+fi
+
+# workstation theme
 if [ "$(id -u)" == "0" ] ; then
 	PS1='\[\e[1;31m\]\u\[\e[1;32m\]@\h\[\e[1;31m\] \W \$\[\e[m\] '
 else
