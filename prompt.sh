9b30ec24c7d58bd04acad7c79e61a68d7cd3f318
empthollow <empthollow@gmail.com>
2024-12-31 11:41:57 -0600

    prompt.sh update

--- prompt.sh
+++ prompt.sh
@@ -1,7 +1,12 @@
 #/bin/bash
 # place in /etc/profile.d/
+
+# server machine user colors
+# PS1="\[\e[1;33m\]\u\[\e[1;32m\]@\h \[\e[1;32m\]\W \$\[\e[m\] "
+
+# client machine colors
 if [ "$(id -u)" == "0" ] ; then
-	PS1='\[\e[1;31m\]\u\[\e[1;32m\]@\h\[\e[1;32m\] \W \$\[\e[m\] '
+	PS1='\[\e[1;31m\]\u\[\e[1;32m\]@\h\[\e[1;31m\] \W \$\[\e[m\] '
 else
-	PS1="\[\e[1;33m\]\u\[\e[1;32m\]@\h \[\e[1;32m\]\W \$\[\e[m\] "
+	PS1="\[\e[1;33m\]\u\[\e[1;34m\]@\h\[\e[1;33m\] \W \$\[\e[m\] "
 fi
